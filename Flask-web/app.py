93467e68bc776961a892d95c9ef78274d5854e23
mosafari <m.safari2680@gmail.com>
2022-11-28 16:59:59 +0330

    adding resource pages

--- Flask-web/app.py
+++ Flask-web/app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-   return render_template('main.html')
+   dicts ={'Python': url_for('PY'),'LPI':url_for('LPI'),'Flask':url_for('Flaskre'),'Django': url_for('django'),'main':url_for('main') }
+   print(dicts['main'])
+   return render_template('main.html', ur=dicts)
+
+@app.route('/LPI')
+def LPI():
+   return render_template('resourcelpi.html')
+
+@app.route('/Python')
+def PY():
+   return render_template('resourcepy.html')
+
+@app.route('/Flask')
+def Flaskre():
+   return render_template('resourceflask.html')
+
+@app.route('/Django')
+def django():
+   return render_template('resourcedjango.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
